6bb04336c50e78107dfdb586629ada0edce55c5f
duanlei <455483293@qq.com>
2015-12-22 14:45:14 +0800

    添加图片位置交互动画

--- app/src/main/java/com/duanlei/pindu/view/GamePintuLayout.java
+++ app/src/main/java/com/duanlei/pindu/view/GamePintuLayout.java
@@ -7,8 +7,11 @@ import android.graphics.Color;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.duanlei.pindu.R;
 import com.duanlei.pindu.utils.ImagePiece;
@@ -50,11 +53,16 @@ public class GamePintuLayout extends RelativeLayout implements View.OnClickListe
 
     private boolean once;
 
-
     /**
      * 游戏面板宽度
      */
     private int mWidth;
+
+    public interface GamePintuListener {
+        void nextLevel(int nextLevel);
+        void timeChanged(int currentTime);
+        void gameOver();
+    }
 
     public GamePintuLayout(Context context) {
         this(context, null);
@@ -183,13 +191,15 @@ public class GamePintuLayout extends RelativeLayout implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        if (isAniming)
+            return;
+
         //两次点击同一个item
         if (mFirst == v) {
             mFirst.setColorFilter(null);
             mFirst = null;
             return;
         }
-
 
         if (mFirst == null) {
             mFirst = (ImageView)v;
@@ -201,32 +211,145 @@ public class GamePintuLayout extends RelativeLayout implements View.OnClickListe
         }
     }
 
+
+    /**
+     * 动画层
+     */
+    private RelativeLayout mAnimLayout;
+
+    private boolean isAniming;
+
+
     /**
      * 交换item
      */
     private void exchangeView() {
         mFirst.setColorFilter(null);
 
-        String firstTag = (String) mFirst.getTag();
+        setUpAnimLayout();
 
-        String secondTag = (String) mSecond.getTag();
+        final String firstTag = (String) mFirst.getTag();
 
-        String[] firstParams = firstTag.split("_");
-        String[] secondParams = secondTag.split("_");
+        ImageView first = new ImageView(getContext());
+        final Bitmap firstBitmap = mItemBitmaps.get(getImageIdByTag(firstTag)).getBitmap();
 
-        Bitmap firstBitmap = mItemBitmaps.get(Integer.valueOf(firstParams[0]))
-                .getBitmap();
+        first.setImageBitmap(firstBitmap);
+        LayoutParams lp = new LayoutParams(mItemWidth, mItemWidth);
+        lp.leftMargin = mFirst.getLeft() - mPadding;
+        lp.topMargin = mFirst.getTop() - mPadding;
+        first.setLayoutParams(lp);
+        mAnimLayout.addView(first);
 
-        Bitmap secondBitmap = mItemBitmaps.get(Integer.valueOf(secondParams[0]))
-                .getBitmap();
 
-        mSecond.setImageBitmap(firstBitmap);
-        mFirst.setImageBitmap(secondBitmap);
+        final String secondTag = (String) mSecond.getTag();
 
-        mFirst.setTag(secondTag);
-        mSecond.setTag(firstTag);
+        ImageView second = new ImageView(getContext());
 
-        mFirst = mSecond = null;
+        final Bitmap secondBitmap = mItemBitmaps.get(getImageIdByTag(secondTag)).getBitmap();
+        second.setImageBitmap(secondBitmap);
+        LayoutParams lp2 = new LayoutParams(mItemWidth, mItemWidth);
+        lp2.leftMargin = mSecond.getLeft() - mPadding;
+        lp2.topMargin = mSecond.getTop() - mPadding;
+        second.setLayoutParams(lp2);
+        mAnimLayout.addView(second);
+
+        //设置动画
+
+        TranslateAnimation animation = new TranslateAnimation(0,
+                mSecond.getLeft() - mFirst.getLeft(), 0, mSecond.getTop() - mFirst.getTop());
+
+        animation.setDuration(300);
+        animation.setFillAfter(true);
+        first.startAnimation(animation);
+
+        TranslateAnimation animationSecond = new TranslateAnimation(0,
+                mFirst.getLeft() - mSecond.getLeft(), 0, mFirst.getTop() - mSecond.getTop());
+
+        animationSecond.setDuration(300);
+        animationSecond.setFillAfter(true);
+        second.startAnimation(animationSecond);
+
+        //动画监听
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                mFirst.setVisibility(View.INVISIBLE);
+                mSecond.setVisibility(View.INVISIBLE);
+                isAniming = true;
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                mSecond.setImageBitmap(firstBitmap);
+                mFirst.setImageBitmap(secondBitmap);
+
+                mFirst.setTag(secondTag);
+                mSecond.setTag(firstTag);
+
+                mFirst.setVisibility(VISIBLE);
+                mSecond.setVisibility(VISIBLE);
+
+                mFirst = mSecond = null;
+                mAnimLayout.removeAllViews();
+
+                //判断游戏是否成功
+                checkSuccess();
+
+                isAniming = false;
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+
+    /**
+     * 判断用户游戏是否成功
+     */
+    private void checkSuccess() {
+        boolean isSuccess = true;
+
+        for (int i = 0; i < mGamePintuItmes.length; i ++) {
+            ImageView imageView = mGamePintuItmes[i];
+
+            if (getImageIndexByTag((String) imageView.getTag()) != i) {
+                isSuccess = false;
+            }
+        }
+
+        if (isSuccess) {
+            Toast.makeText(getContext(), "Success, level up!!!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    /**
+     * 根据tag获取item的id
+     * @param tag
+     * @return
+     */
+    public int getImageIdByTag(String tag) {
+        String[] split = tag.split("_");
+        return Integer.valueOf(split[0]);
+    }
+
+
+    public int getImageIndexByTag(String tag) {
+        String[] split = tag.split("_");
+        return Integer.valueOf(split[1]);
+    }
+
+
+    /**
+     * 构造动画层
+     */
+    private void setUpAnimLayout() {
+        if (mAnimLayout == null) {
+            mAnimLayout = new RelativeLayout(getContext());
+            addView(mAnimLayout);
+        }
     }
 }
 
